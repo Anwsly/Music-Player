444a66c6b85be6b2c840692a59f1bb82aa06f820
Anwsly <18893476304@163.com>
2016-12-03 23:59:41 +0800

    all file

---
+++ musicPlayer/js/pc.js
@@ -0,0 +1,17 @@
+//播放控制
+	
+	function ok(){
+		document.getElementById("ifr").src='html/one.html';
+	}
+function ok2(){
+		document.getElementById("ifr").src='html/two.html';
+	}
+	function ok3(){
+		document.getElementById("ifr").src='html/three.html';
+	}
+	function ok4(){
+		document.getElementById("ifr").src='html/four.html';
+	}
+	function Ctr(){
+		//document.getElementById("playOne").style.backgroundColor="black";
+	}
